a3094778c95376f3f5376b5b0fb12d04924d26c1
wagnerpuah <vinhato.wagner@puah.com.br>
2025-01-19 17:37:19 -0300

    feat: allow updating user with the same email as before

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,9 +1,16 @@
 import User from '#models/user.module'
 import { userValidator } from '#validators/user'
+import { Exception } from '@adonisjs/core/exceptions'
 import { Infer } from '@vinejs/vine/types'
 
 export default class UsersService {
   async create(data: Infer<typeof userValidator>) {
+    const existingUser = await User.query().where('email', data.email).first()
+    if (existingUser) {
+      throw new Exception('Email já está em uso.', {
+        status: 400,
+      })
+    }
     const newUser = await User.create(data)
     return newUser
   }
@@ -20,6 +27,12 @@ export default class UsersService {
 
   async update(id: number, data: Infer<typeof userValidator>) {
     const user = await User.findOrFail(id)
+    const existingUser = await User.query().where('email', data.email).first()
+    if (existingUser && existingUser.id !== user.id) {
+      throw new Exception('Email já está em uso.', {
+        status: 400,
+      })
+    }
     user.merge(data)
     await user.save()
     return user
--- app/validators/user.ts
+++ app/validators/user.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const userValidator = vine.compile(
   vine.object({
     name: vine.string().maxLength(100),
-    age: vine.number().min(18),
+    age: vine.number().min(18).max(130),
     email: vine.string().email(),
   })
 )
