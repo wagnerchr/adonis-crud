ec817de55293d10e9acb2a7021b876022d0d8168
wagnerpuah <vinhato.wagner@puah.com.br>
2025-01-20 00:51:05 -0300

    chore: fix deploy issues

--- bin/server.ts
+++ bin/server.ts
@@ -15,11 +15,17 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
     app.booting(async () => {
       await import('#start/env')
     })
+
     app.listen('SIGTERM', () => app.terminate())
     app.listenIf(app.managedByPm2, 'SIGINT', () => app.terminate())
   })
   .httpServer()
-  .start()
+  .start((handle: any) => {
+    const port = process.env.PORT || 10000
+    return handle.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on http://0.0.0.0:${port}`)
+    })
+  })
   .catch((error) => {
     process.exitCode = 1
     prettyPrintError(error)
